ba6ec537119caa17826232e24f1dc9441d88f9d5
Retr0327 <philcoke35@gmail.com>
2022-09-24 17:03:08 +0800

    Add validate

---
+++ src/api/validations/validator.ts
@@ -0,0 +1,19 @@
+import { Middleware } from '@koa/router';
+import { SchemaOf, ValidationError } from 'yup';
+
+function validate<T>(schema: SchemaOf<T>): Middleware {
+  return async (ctx, next) => {
+    try {
+      await schema.validate(ctx.request.body);
+      return await next();
+    } catch (error) {
+      const errors = error as ValidationError;
+      console.log(errors); // eslint-disable-line no-console
+      ctx.status = 422;
+      ctx.body = { status: 'failed', msg: 'invalid request body' };
+    }
+    return null;
+  };
+}
+
+export default validate;
