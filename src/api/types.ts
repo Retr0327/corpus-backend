263f2b92a944507e56d47014fbb0c845dbaa161a
Retr0327 <philcoke35@gmail.com>
2022-09-24 17:08:30 +0800

    Add BlacklabResponse

--- src/api/types.ts
+++ src/api/types.ts
@@ -12,3 +12,84 @@ export type CorpusRequest = {
   page: number;
   fetchNumber: number;
 };
+
+// ------ blacklab types ------
+
+export interface BlacklabResponse {
+  summary: Summary;
+  hits: Hit[] | [];
+  docInfos: DocInfos | {};
+}
+
+export interface Hit {
+  docPid: string;
+  start: number;
+  end: number;
+  left: HitData;
+  match: HitData;
+  right: HitData;
+}
+
+export interface HitData {
+  punct: string[];
+  pos: string[];
+  word: string[];
+}
+
+export interface DocInfos {
+  [key: string]: {
+    fromInputFile: string[];
+    year: string[];
+    author: string[];
+    media: string[];
+    doc_id: string[];
+    board: string[];
+    lengthInTokens: number;
+    mayView: boolean;
+  };
+}
+
+export interface Summary {
+  searchParam: SearchParam;
+  searchTime: number;
+  countTime: number;
+  windowFirstResult: number;
+  requestedWindowSize: number;
+  actualWindowSize: number;
+  windowHasPrevious: boolean;
+  windowHasNext: boolean;
+  stillCounting: boolean;
+  numberOfHits: number;
+  numberOfHitsRetrieved: number;
+  stoppedCountingHits: boolean;
+  stoppedRetrievingHits: boolean;
+  numberOfDocs: number;
+  numberOfDocsRetrieved: number;
+  docFields: DocFields;
+  metadataFieldDisplayNames: MetadataFieldDisplayNames;
+}
+
+export interface DocFields {
+  pidField: string;
+  titleField: string;
+  authorField: string;
+  dateField: string;
+}
+
+export interface MetadataFieldDisplayNames {
+  author: string;
+  board: string;
+  doc_id: string;
+  fromInputFile: string;
+  media: string;
+  year: string;
+}
+
+export interface SearchParam {
+  filter: string;
+  first: string;
+  indexname: string;
+  number: string;
+  patt: string;
+  wordsaroundhit: string;
+}
