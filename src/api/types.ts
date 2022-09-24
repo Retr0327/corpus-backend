0622105a54f6ee142cdd0d944e1fbc11be8917c3
Retr0327 <philcoke35@gmail.com>
2022-09-24 17:08:14 +0800

    Add CorpusRequest

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,1 +1,14 @@
 export type PostType = 'title' | 'body' | 'commentAll' | 'commentPos' | 'commentNeu' | 'commentNeg';
+
+export type CorpusRequest = {
+  word: string;
+  cqlEnable: boolean;
+  media: string | null;
+  postType: PostType;
+  boards: string | null;
+  start: string;
+  end: string;
+  windowSize: string;
+  page: number;
+  fetchNumber: number;
+};
