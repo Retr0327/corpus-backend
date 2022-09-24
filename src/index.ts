b3af6113bcf620f3704a1150ed6b7d0d5f6a616f
Retr0327 <philcoke35@gmail.com>
2022-09-24 16:58:13 +0800

    Update console.log

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log();
+  console.log(`⚡️[server] Listening on port ${PORT}`);
 });
