c9f0fe758e91caf187ee334c5c80bbfda20d8990
Retr0327 <philcoke35@gmail.com>
2022-10-01 10:12:39 +0800

    Add buildParams

---
+++ src/api/middlewares/corpus/index.ts
@@ -0,0 +1,4 @@
+import isCQL from './cql';
+import buildParams from './params';
+
+export { isCQL, buildParams };
---
+++ src/api/middlewares/corpus/params.ts
@@ -0,0 +1,45 @@
+import { buildTextPattern, buildFilterString } from '@utils/blacklab/requestBody';
+import { CorpusRequest } from 'types';
+import { Middleware } from '@koa/router';
+
+type Service = 'query' | 'csv';
+
+const buildParams =
+  (service: Service): Middleware =>
+  async (ctx, next) => {
+    const { word, cqlEnable, media, postType, boards, start, end, page, fetchNumber, windowSize } =
+      ctx.request.body as CorpusRequest;
+
+    const textPattern = buildTextPattern(cqlEnable, word, postType);
+    const filterString = buildFilterString(media, boards, start, end);
+
+    let requestBody: { [key: string]: any } = {
+      patt: textPattern,
+      filter: filterString,
+    };
+
+    switch (service) {
+      case 'csv':
+        requestBody = {
+          outputformat: 'csv',
+          indexname: 'indexes',
+          ...requestBody,
+        };
+        break;
+
+      default:
+        requestBody = {
+          outputformat: 'json',
+          first: (page * fetchNumber).toString(),
+          number: fetchNumber.toString(),
+          wordsaroundhit: windowSize,
+          ...requestBody,
+        };
+        break;
+    }
+
+    ctx.blacklabReuqestBody = requestBody;
+    return next();
+  };
+
+export default buildParams;
