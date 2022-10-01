d60fe9d97ed156ed81df69a77568c05f7e43da07
Retr0327 <philcoke35@gmail.com>
2022-10-01 13:07:55 +0800

    Update handleQuery

--- src/api/controllers/query/index.ts
+++ src/api/controllers/query/index.ts
@@ -3,18 +3,23 @@ import { Middleware } from '@koa/router';
 import request from '@utils/blacklab/request';
 
 const handleQuery: Middleware = async (ctx) => {
-  const { blacklabReuqestBody } = ctx;
-  const blacklabParams = new URLSearchParams(blacklabReuqestBody).toString();
-  const response = await request<BlacklabResponse>(`hits/?${blacklabParams}`);
+  try {
+    const { blacklabReuqestBody } = ctx;
+    const blacklabParams = new URLSearchParams(blacklabReuqestBody).toString();
+    const response = await request<BlacklabResponse>(`hits/?${blacklabParams}`);
 
-  if (response.hits.length === 0) {
+    if (response.hits.length === 0) {
+      ctx.status = 200;
+      ctx.body = { status: 'success', msg: 'no hits' };
+      return;
+    }
+
     ctx.status = 200;
-    ctx.body = { status: 'success', msg: 'no hits' };
-    return;
+    ctx.body = { status: 'success', data: { ...response } };
+  } catch (error) {
+    ctx.status = 404;
+    ctx.body = { status: 'failed', msg: 'not found' };
   }
-
-  ctx.status = 200;
-  ctx.body = { status: 'success', data: { ...response } };
 };
 
 export default handleQuery;
