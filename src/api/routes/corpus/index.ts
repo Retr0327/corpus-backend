8beea70a41366658e144e56d97a53d45c273aa24
Retr0327 <philcoke35@gmail.com>
2022-10-01 12:58:58 +0800

    Update `handleQuery` import path

--- src/api/routes/corpus/index.ts
+++ src/api/routes/corpus/index.ts
@@ -1,6 +1,7 @@
 import Router from '@koa/router';
+import handleQuery from '@controllers/query';
+import handleCSVDownload from '@controllers/csv';
 import { isCQL, buildParams } from '@middlewares/corpus';
-import { handleQuery, handleCSVDownload } from '@controllers';
 import { validateQuery, validateCSVDownload } from '@validations/blacklab';
 
 const router = new Router({ prefix: '/corpus' });
