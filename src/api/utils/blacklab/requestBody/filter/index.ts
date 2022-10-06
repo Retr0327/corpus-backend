48bd53f46d1ece488e16a8299d249a4ec7276e11
Retr0327 <philcoke35@gmail.com>
2022-10-06 21:10:05 +0800

    Update board format

--- src/api/utils/blacklab/requestBody/filter/index.ts
+++ src/api/utils/blacklab/requestBody/filter/index.ts
@@ -1,5 +1,10 @@
 function buildFilterString(media: string | null, board: string | null, start: string, end: string) {
-  const boardFilter = board ? `board:(${board}) AND ` : '';
+  const boardFilter = board
+    ? `board:(${board
+        .split('+')
+        .map((value) => `${value.trim()}-dcard`)
+        .join(' + ')}) AND `
+    : '';
   const mediaFilter = media ? `media:(${media}) AND ` : '';
   const yearFilter = `year:[${start} TO ${end}]`;
 
