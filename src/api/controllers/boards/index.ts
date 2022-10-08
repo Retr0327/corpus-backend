8b7ea8d6699d45e5ac65c4a2a75afea37249dc31
Retr0327 <philcoke35@gmail.com>
2022-10-08 23:26:28 +0800

    Update handleGetBoards

--- src/api/controllers/boards/index.ts
+++ src/api/controllers/boards/index.ts
@@ -1,12 +1,23 @@
 import { Boards } from 'types';
 import { Middleware } from '@koa/router';
 import request from '@utils/blacklab/request';
+import generateBoards from './utils';
 
 const handleGetBoards: Middleware = async (ctx) => {
   const response = await request<Boards>('fields/board?outputformat=json');
+  const boards = Object.keys(response.fieldValues);
+
+  const isDcard = /.*(?=-dcard)/;
+  const isPtt = /.*(?=-ptt)/;
 
   ctx.status = 200;
-  ctx.body = { status: 'success', data: response.fieldValues };
+  ctx.body = {
+    status: 'success',
+    data: {
+      ptt: generateBoards(boards, isPtt),
+      dcard: generateBoards(boards, isDcard),
+    },
+  };
 };
 
 export default handleGetBoards;
