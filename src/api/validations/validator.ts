049757ebc282f7a75931cfd7b713d954c1c3c576
Retr0327 <philcoke35@gmail.com>
2022-10-09 19:40:13 +0800

    Change method to `validateSync`

--- src/api/validations/validator.ts
+++ src/api/validations/validator.ts
@@ -1,18 +1,16 @@
+import { SchemaOf } from 'yup';
 import { Middleware } from '@koa/router';
-import { SchemaOf, ValidationError } from 'yup';
 
 function validate<T>(schema: SchemaOf<T>): Middleware {
-  return async (ctx, next) => {
+  return (ctx, next) => {
     try {
-      await schema.validate(ctx.request.body);
-      return next(); // eslint-disable-line @typescript-eslint/return-await
+      schema.validateSync(ctx.request.body);
+      return next();
     } catch (error) {
-      const errors = error as ValidationError;
-      console.log(errors); // eslint-disable-line no-console
       ctx.status = 422;
       ctx.body = { status: 'failed', msg: 'invalid request body' };
+      return null;
     }
-    return null;
   };
 }
 
