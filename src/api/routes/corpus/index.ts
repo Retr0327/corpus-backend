2e81d27a4a642fdc2fea823b86322aff240a1678
Retr0327 <philcoke35@gmail.com>
2022-10-09 20:02:27 +0800

    Inject `hasBoards` into boards route

--- src/api/routes/corpus/index.ts
+++ src/api/routes/corpus/index.ts
@@ -2,7 +2,7 @@ import Router from '@koa/router';
 import handleQuery from '@controllers/query';
 import handleCSVDownload from '@controllers/csv';
 import handleGetBoards from '@controllers/boards';
-import { isCQL, buildParams } from '@middlewares/corpus';
+import { isCQL, hasBoards, buildParams } from '@middlewares/corpus';
 import { validateQuery, validateCSVDownload } from '@validations/blacklab';
 
 const router = new Router({ prefix: '/corpus' });
@@ -11,6 +11,6 @@ router.use(isCQL());
 
 router.post('/', validateQuery, buildParams('query'), handleQuery);
 router.post('/csv', validateCSVDownload, buildParams('csv'), handleCSVDownload);
-router.get('/boards', handleGetBoards);
+router.get('/boards', hasBoards(), handleGetBoards);
 
 export default router;
