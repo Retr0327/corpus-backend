9f13597fc1ee95eb66c24f683626c93465320b1d
Retr0327 <philcoke35@gmail.com>
2022-10-13 14:25:37 +0800

    Cache query result

--- src/api/controllers/query/index.ts
+++ src/api/controllers/query/index.ts
@@ -1,6 +1,7 @@
 import { BlacklabResponse } from 'types';
 import { Middleware } from '@koa/router';
 import request from '@utils/blacklab/request';
+import cacheQueryResult from './cache';
 
 const handleQuery: Middleware = async (ctx) => {
   try {
@@ -14,8 +15,9 @@ const handleQuery: Middleware = async (ctx) => {
       return;
     }
 
+    await cacheQueryResult(blacklabParams, { data: JSON.stringify(response) }, 0.2);
     ctx.status = 200;
-    ctx.body = { status: 'success', data: { ...response } };
+    ctx.body = { status: 'success', data: response };
   } catch (error) {
     ctx.status = 404;
     ctx.body = { status: 'failed', msg: 'not found' };
