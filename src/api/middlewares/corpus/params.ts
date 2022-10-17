7c871235b59b6503d7fecfad459900e4158279d2
Retr0327 <philcoke35@gmail.com>
2022-10-17 18:19:27 +0800

    Update buildParams

--- src/api/middlewares/corpus/params.ts
+++ src/api/middlewares/corpus/params.ts
@@ -1,14 +1,12 @@
 import { buildTextPattern, buildFilterString } from '@utils/blacklab/requestBody';
-import { CorpusRequest } from 'types';
+import { RequestBody, CorpusRequest } from 'types';
 import { Middleware } from '@koa/router';
 
-type Service = 'query' | 'csv';
-
 const buildParams =
-  (service: Service): Middleware =>
+  (service: 'query' | 'csv'): Middleware<{}, RequestBody<CorpusRequest>> =>
   async (ctx, next) => {
     const { word, cqlEnable, media, postType, boards, start, end, page, fetchNumber, windowSize } =
-      ctx.request.body as CorpusRequest;
+      ctx.request.body;
 
     if (Number(start) > Number(end)) {
       ctx.status = 422;
@@ -16,32 +14,25 @@ const buildParams =
       return null;
     }
 
-    const textPattern = buildTextPattern(cqlEnable, word, postType);
-    const filterString = buildFilterString(media, boards, start, end);
-
     let requestBody: { [key: string]: any } = {
-      patt: textPattern,
-      filter: filterString,
+      patt: buildTextPattern(cqlEnable, word, postType),
+      filter: buildFilterString(media, boards, start, end),
     };
 
-    switch (service) {
-      case 'csv':
-        requestBody = {
-          outputformat: 'csv',
-          indexname: 'indexes',
-          ...requestBody,
-        };
-        break;
-
-      default:
-        requestBody = {
-          outputformat: 'json',
-          first: (page * fetchNumber - fetchNumber).toString(),
-          number: fetchNumber.toString(),
-          wordsaroundhit: windowSize,
-          ...requestBody,
-        };
-        break;
+    if (service === 'query') {
+      requestBody = {
+        ...requestBody,
+        outputformat: 'json',
+        first: (page * fetchNumber - fetchNumber).toString(),
+        number: fetchNumber.toString(),
+        wordsaroundhit: windowSize,
+      };
+    } else {
+      requestBody = {
+        ...requestBody,
+        outputformat: 'csv',
+        indexname: 'indexes',
+      };
     }
 
     ctx.blacklabReuqestBody = requestBody;
