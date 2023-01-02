efd8f8660c773400218aa3c56bb90c2d07f77def
Retr0327 <philcoke35@gmail.com>
2023-01-03 00:12:00 +0800

    Implement hset function for Redis hash storage with expiration

--- src/api/utils/redis/index.ts
+++ src/api/utils/redis/index.ts
@@ -1,6 +1,13 @@
-function getExpireHour(hour: number) {
+import redis from '@models/redis';
+
+function setExpireHour(hour: number) {
   const oneHour = 60 * 60;
   return Math.ceil(oneHour * hour);
 }
 
-export default getExpireHour;
+async function hset(key: string, payload: object, hour: number = 0.5) {
+  const expire = setExpireHour(hour);
+  await redis.multi().hset(key, payload).expire(key, expire).exec();
+}
+
+export { setExpireHour, hset };
