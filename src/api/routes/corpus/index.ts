28038008dce1dabc8415fb280f93b1a272ee21b8
Retr0327 <philcoke35@gmail.com>
2023-01-03 00:27:58 +0800

    Refactor corpus routes

--- src/api/routes/corpus/index.ts
+++ src/api/routes/corpus/index.ts
@@ -1,16 +1,22 @@
 import Router from '@koa/router';
-import handleQuery from '@controllers/query';
-import handleCSVDownload from '@controllers/csv';
 import handleGetBoards from '@controllers/boards';
-import { isCQL, hasBoards, buildParams, hasQueryResult } from '@middlewares/corpus';
-import { validateQuery, validateCSVDownload } from '@validations/blacklab';
+import hasBoards from '@middlewares/corpus/boards';
+import isCQL from '@middlewares/corpus/concordance';
+import createQuery from '@middlewares/corpus/concordance/query';
+import hasConcordance from '@middlewares/corpus/concordance/cache';
+import handleGetConcordance, { validateGetConcordance } from '@controllers/concordance';
 
 const router = new Router({ prefix: '/corpus' });
 
-router.use(isCQL());
+router.post(
+  '/concordance',
+  validateGetConcordance,
+  isCQL,
+  createQuery('concordance'),
+  hasConcordance,
+  handleGetConcordance,
+);
 
-router.post('/', validateQuery, buildParams('query'), hasQueryResult, handleQuery);
-router.post('/csv', validateCSVDownload, buildParams('csv'), handleCSVDownload);
 router.get('/boards', hasBoards, handleGetBoards);
 
 export default router;
