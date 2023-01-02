05314ef0c7a7581250b3d5239dc2575656859873
Retr0327 <philcoke35@gmail.com>
2023-01-02 23:55:45 +0800

    Update production blacklab api url

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,9 +1,12 @@
-const PREFIX = process.env.NODE_ENV === 'production' ? '/' : '/api';
+const nodeEnv = process.env.NODE_ENV;
+const blacklabUrl = process.env.BLACKLAB_URL;
+const isProduction = nodeEnv === 'production';
 
-const { BLACKLAB_URL } = process.env;
-
-if (!BLACKLAB_URL) {
+if (!blacklabUrl) {
   throw new Error('BLACKLAB_URL undefined');
 }
+
+const PREFIX = isProduction ? '/api/v1' : '/service/api/v1';
+const BLACKLAB_URL = isProduction ? 'http://nginx:80/service/blacklab/api/v1' : blacklabUrl;
 
 export { PREFIX, BLACKLAB_URL };
